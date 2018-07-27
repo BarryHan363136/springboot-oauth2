2371fce3008be181ccebd4c45703000b82a72616
Tongshan Han <Tongshan.Han@partner.bmw.com>
2018-07-27 15:34:23 +0800

    base config

---
+++ src/main/java/com/bmw/azure/oauth2/starter/ApplicationStarter.java
@@ -0,0 +1,25 @@
+package com.bmw.azure.oauth2.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * TaskApplication [spring boot] 主方法
+ *
+ * 启动方式，右键->run/debug->Spring Boot App
+ *
+ * @author
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages={"com.bmw.azure.oauth2"})
+@EnableCaching
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+
+}
